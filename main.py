1481b97e3adb2b569d0682b648ea04ffe8740108
nguyntuan-dev <nguyenanhtuan18112006@gmail.com>
2026-04-21 00:06:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,7 @@ from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
 
-# Load environment variables
-load_dotenv()
+
 
 from cedict_parser import cedict
 from database import engine, Base, get_db
